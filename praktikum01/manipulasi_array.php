3f147e67a4996724de05ce00d763403247fc8783
kamilablqs <114552156+kamilablqs@users.noreply.github.com>
2023-02-25 20:10:59 +0700

    praktikum & tugas 1

---
+++ praktikum01/manipulasi_array.php
@@ -0,0 +1,29 @@
+<?php
+
+$arrayBuah = ["Jeruk","Manggis","Apel","Durian"];
+
+//mengurutkan array
+sort($arrayBuah);
+
+//menghapus value array yang paling akhir
+array_pop($arrayBuah);
+
+//menghapus variabel ataupun value array
+unset($arrayBuah[0]);
+
+//menambahkan value array di paling akhir
+array_push($arrayBuah,"Buah Naga");
+
+//menghapus value/data array di paling awal
+array_shift($arrayBuah);
+
+//menambakhan value array di paling awal
+array_unshift($arrayBuah, "Semangka");
+
+// mengubah data/value array dengan spesifik key/index
+$arrayBuah[1] = "Anggur";
+
+
+foreach($arrayBuah as $buah) {
+    echo  "<br>" . $buah;
+}

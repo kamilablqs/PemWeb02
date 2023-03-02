fba32494b90988ad536ea1ace7abb3dd443ca591
kamilablqs <114552156+kamilablqs@users.noreply.github.com>
2023-03-02 15:22:56 +0700

    Praktikum02

---
+++ praktikum02/condisional_switchcase.php
@@ -0,0 +1,14 @@
+<?php
+$nasi = "ditumbuk";
+
+switch($nasi) {
+    case "digoreng" :
+        echo "ya, ini digoreng";
+        break;
+    case "dibakar" :
+        echo "ya, ini dibakar";
+        break;
+    default :
+        echo "Nasi Biasa";
+        break;
+}
---
+++ praktikum02/kondisional_ifelse.php
@@ -0,0 +1,10 @@
+<?php
+$nasi = "dibakar";
+
+if($nasi == "digoreng"){
+    echo "ya, ini di goreng";
+} elseif($nasi == "dibakar"){
+    echo "ya, ini di bakar";
+}else {
+    echo "Nasi biasa";
+}

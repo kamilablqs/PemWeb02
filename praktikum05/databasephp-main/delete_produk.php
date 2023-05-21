c7bc9fc6185a93c889a23fdab324022552f2d1ae
kamilablqs <114552156+kamilablqs@users.noreply.github.com>
2023-05-21 10:22:32 +0700

    Delete praktikum05 directory

--- praktikum05/databasephp-main/delete_produk.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dbkoneksi.php';
-
-$iddel = $_GETp['iddel'];
-$query = "DELETE FROM produk WHERE id = ?";
-$delete = $dbh->prepare($query);
-$delete->execute([$iddel]);
-header('location:list_produk.php');
-?>

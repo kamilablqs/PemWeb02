2cb5f75dc4da6b9553e4e19238d33f058852cdbc
kamilablqs <114552156+kamilablqs@users.noreply.github.com>
2023-05-21 10:17:56 +0700

    praktikum02

---
+++ praktikum02/project.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+</head>
+<body>
+    <form action="project_proses.php" method="post">
+        <label for="">Username</label><br>
+        <input type="text" name="username"><br><br>
+
+        <label for="">Password</label><br>
+        <input type="password" name="password"><br><br>
+        
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
